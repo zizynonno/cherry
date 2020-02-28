342346bf86328f62f85502a225bdd93bf47c1761
zizynonno <zizynonno@gmail.com>
2020-02-28 12:37:07 +0900

    module1

---
+++ lib/module.rb
@@ -0,0 +1,43 @@
+module Loggable
+  private
+
+  def log(text)
+    puts "[LOG] #{text}"
+  end
+end
+
+
+class Product
+  include Loggable
+  def title
+    log 'title is called'
+    'A great movie'
+  end
+
+  # private
+
+  # def log(text)
+  #   puts "[LOG] #{text}"
+  # end
+end
+
+class User
+  include Loggable
+  def name
+    log 'name is called'
+    'Alice'
+  end
+
+    # private
+
+    # def log(text)
+    #   puts "[LOG] #{text}"
+    # end
+end
+
+product = Product.new
+product.title
+user = User.new
+user.name
+
+# product.log 'public?'
