b4730e59ab496e2e3d79f7b2c608116ec57358ba
zizynonno <zizynonno@gmail.com>
2020-03-02 18:42:14 +0900

    yield

---
+++ lib/yield.rb
@@ -0,0 +1,29 @@
+def greeting
+  puts "good morning"
+  if block_given?
+    yield
+  end
+  puts "good evening"
+end
+
+greeting
+
+puts "---------"
+
+greeting do
+  puts "hello"
+end
+
+puts "---------"
+
+def greeting2
+  puts 'おはよう'
+  text = yield 'こんにちは'
+  puts text
+  puts 'こんにちは'
+end
+
+greeting2 do |text|
+  text * 2
+end
+
