10e7ad59e024bd0b1abbe8c856452170e75b7c4a
zizynonno <zizynonno@gmail.com>
2020-03-02 17:51:55 +0900

    module4

--- lib/module.rb
+++ lib/module.rb
@@ -42,3 +42,20 @@ user.name
 
 # product.log 'public?'
 
+# モジュールの特異メソッドとしてsqrtメソッドを利用する
+a = Math.sqrt(2)
+puts a
+
+class Calculation
+  include Math
+   #ミックスインとしてMathモジュールのsqrtメソッドを使う
+  def calc_sqrt(n)
+    sqrt(n)
+  end
+end
+
+calculation = Calculation.new
+puts calculation.calc_sqrt(2)
+
+#Kernelモジュールのメソッドはモジュール関数になっているため、特異メソッドとして呼び出せる
+Kernel.puts "Hello"
